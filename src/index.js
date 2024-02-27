c0704387eb5c1632199c01be59facdc69c608bcf
ashish.singh <ashish.singh@webengage.com>
2024-02-27 22:29:42 +0530

    ran postman

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose';
 import {connectDB} from './db/index.js'
+import {app} from './app.js'
 
 dotenv.config({
     path:'./env'
